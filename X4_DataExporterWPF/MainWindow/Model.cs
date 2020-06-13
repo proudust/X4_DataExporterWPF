426bb8045a2c3f45ecaa6f68fa59fea3e42d2de4
Proudust <proudust@gmail.com>
2020-06-13 22:39:46 +0900

    ViewModelをReactivePropertyでリファクタリング

--- X4_DataExporterWPF/MainWindow/Model.cs
+++ X4_DataExporterWPF/MainWindow/Model.cs
@@ -1,195 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
 using System.IO;
 using System.Linq;
 using System.Windows;
-using System.Windows.Threading;
 using System.Xml.XPath;
 using CommandLine;
 using LibX4.FileSystem;
 using LibX4.Lang;
-using X4_ComplexCalculator.Common;
 using X4_DataExporterWPF.Common;
 using X4_DataExporterWPF.Export;
 
 namespace X4_DataExporterWPF.MainWindow
 {
-    class Model : INotifyPropertyChangedBace
+    class Model
     {
-        #region メンバ
-        /// <summary>
-        /// 入力元フォルダパス
-        /// </summary>
-        private string _InDirPath;
-
-
-        /// <summary>
-        /// 出力先ファイルパス
-        /// </summary>
-        private string _OutFilePath;
-
-
-        /// <summary>
-        /// ユーザが操作可能か
-        /// </summary>
-        private bool _CanOperation = true;
-
-
-        /// <summary>
-        /// 進捗最大
-        /// </summary>
-        private int _MaxSteps = 100;
-
-
-        /// <summary>
-        /// 現在の進捗
-        /// </summary>
-        private int _CurrentStep = 0;
-        #endregion
-
-        #region プロパティ
-        /// <summary>
-        /// 言語一覧
-        /// </summary>
-        public ObservableRangeCollection<LangComboboxItem> Langages = new ObservableRangeCollection<LangComboboxItem>();
-
-
-        /// <summary>
-        /// 選択された言語
-        /// </summary>
-        public LangComboboxItem SelectedLangage { get; set; }
-
-
-        /// <summary>
-        /// 入力元フォルダパス
-        /// </summary>
-        public string InDirPath
-        {
-            get
-            {
-                return _InDirPath;
-            }
-            set
-            {
-                _InDirPath = value;
-                UpdateLangages();
-                OnPropertyChanged();
-            }
-        }
-
-
-        /// <summary>
-        /// 出力先ファイルパス
-        /// </summary>
-        public string OutFilePath
-        {
-            get
-            {
-                return _OutFilePath;
-            }
-            set
-            {
-                _OutFilePath = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        /// <summary>
-        /// 進捗最大値
-        /// </summary>
-        public int MaxSteps
-        {
-            get
-            {
-                return _MaxSteps;
-            }
-            private set
-            {
-                if (value != _MaxSteps)
-                {
-                    _MaxSteps = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        /// <summary>
-        /// 現在の進捗
-        /// </summary>
-        public int CurrentStep
-        {
-            get
-            {
-                return _CurrentStep;
-            }
-            set
-            {
-                if (value != _CurrentStep)
-                {
-                    _CurrentStep = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-
-        /// <summary>
-        /// ユーザが操作可能か
-        /// </summary>
-        public bool CanOperation
-        {
-            get
-            {
-                return _CanOperation;
-            }
-            private set
-            {
-                if (_CanOperation != value)
-                {
-                    _CanOperation = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-        #endregion
-
         /// <summary>
         /// コンストラクタ
         /// </summary>
-        public Model()
+        public CommandlineOptions GetCommandlineOptions()
         {
             var option = Parser.Default.ParseArguments<CommandlineOptions>(Environment.GetCommandLineArgs());
             if (option.Tag == ParserResultType.Parsed)
             {
                 var parsed = (Parsed<CommandlineOptions>)option;
-
-                InDirPath = parsed.Value.InputDirectory;
-                OutFilePath = parsed.Value.OutputFilePath;
+                return parsed.Value;
             }
+            return new CommandlineOptions();
         }
 
 
         /// <summary>
         /// 言語一覧を更新
         /// </summary>
-        private void UpdateLangages()
+        public IEnumerable<LangComboboxItem> GetLangages(string inDirPath)
         {
             try
             {
-                var catFiles = new CatFile(_InDirPath);
-
+                var catFiles = new CatFile(inDirPath);
                 var xml = catFiles.OpenXml("libraries/languages.xml");
+                var langages = xml.XPathSelectElements("/languages/language")
+                    .Select(x => new LangComboboxItem(int.Parse(x.Attribute("id").Value), x.Attribute("name").Value))
+                    .OrderBy(x => x.ID);
 
-                var langages = xml.XPathSelectElements("/languages/language").Select(x => new LangComboboxItem(int.Parse(x.Attribute("id").Value), x.Attribute("name").Value))
-                                                                             .OrderBy(x => x.ID);
-
-                Langages.Reset(langages);
+                return langages;
             }
             catch (Exception)
             {
-                Langages.Clear();
+                return Enumerable.Empty<LangComboboxItem>();
             }
         }
 
@@ -197,23 +56,25 @@ namespace X4_DataExporterWPF.MainWindow
         /// <summary>
         /// 抽出実行
         /// </summary>
-        public void Export()
+        /// <param name="inDirPath">入力元フォルダパス</param>
+        /// <param name="outFilePath">出力先ファイルパス</param>
+        /// <param name="language">選択された言語</param>
+        /// <returns>現在数と合計数のタプルのイテレータ</returns>
+        public IEnumerable<(int currentStep, int maxSteps)> Export(string inDirPath, string outFilePath, LangComboboxItem language)
         {
 #if !DEBUG
             try
 #endif
             {
-                // ユーザ操作禁止
-                CanOperation = false;
 
-                if (File.Exists(OutFilePath))
+                if (File.Exists(outFilePath))
                 {
-                    File.Delete(OutFilePath);
+                    File.Delete(outFilePath);
                 }
 
-                var catFile = new CatFile(_InDirPath);
+                var catFile = new CatFile(inDirPath);
 
-                var consb = new SQLiteConnectionStringBuilder { DataSource = OutFilePath };
+                var consb = new SQLiteConnectionStringBuilder { DataSource = outFilePath };
                 using var conn = new SQLiteConnection(consb.ToString());
                 conn.Open();
 
@@ -223,7 +84,7 @@ namespace X4_DataExporterWPF.MainWindow
 
                 // 英語をデフォルトにする
                 resolver.LoadLangFile(44);
-                resolver.LoadLangFile(SelectedLangage.ID);
+                resolver.LoadLangFile(language.ID);
 
 
                 var waresXml = catFile.OpenXml("libraries/wares.xml");
@@ -270,13 +131,13 @@ namespace X4_DataExporterWPF.MainWindow
                 };
 
                 // 進捗初期化
-                MaxSteps = exporters.Length;
-                CurrentStep = 0;
+                var maxSteps = exporters.Length;
+                var currentStep = 0;
                 foreach (var exporter in exporters)
                 {
                     exporter.Export(conn);
-                    CurrentStep++;
-                    DoEvents();
+                    currentStep++;
+                    yield return (currentStep, maxSteps);
                 }
 
                 trans.Commit();
@@ -289,27 +150,7 @@ namespace X4_DataExporterWPF.MainWindow
                 var msg = $"■Message\r\n{e.Message}\r\n\r\n■StackTrace\r\n{e.StackTrace}";
                 MessageBox.Show(msg, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            finally
 #endif
-            {
-                // 操作禁止解除
-                CanOperation = true;
-
-                // 進捗初期化
-                CurrentStep = 0;
-            }
-        }
-
-
-        /// <summary>
-        /// 画面更新
-        /// </summary>
-        private void DoEvents()
-        {
-            var frame = new DispatcherFrame();
-            var callback = new DispatcherOperationCallback((obj) => { ((DispatcherFrame)obj).Continue = false; return null; });
-            Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background, callback, frame);
-            Dispatcher.PushFrame(frame);
         }
     }
 }
